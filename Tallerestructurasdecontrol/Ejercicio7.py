d26e74a0ea090d38b61d8911cdb19abb994301f9
Gabriela Montenegro <91491595+Gasanmon@users.noreply.github.com>
2021-10-17 22:22:26 -0500

    Create Ejercicio7.py

---
+++ Tallerestructurasdecontrol/Ejercicio7.py
@@ -0,0 +1,12 @@
+"""
+Entradas
+Metros -> int -> Metros
+Salidas
+Pulgadas -> int -> Pulgadas
+Pies -> int -> Pies
+
+"""
+Metros = int ( input ( "Insertar valor en metros:" ))
+Pulgadas = ( Metros * 39.27 )
+Tartas = ( Pulgadas * 12 )
+print ( "Metros a pulgadas:" + str ( Pulgadas ), "Metros a pies:" + str ( Pies ))
