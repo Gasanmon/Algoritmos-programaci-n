e6b5135e3f52af4740a0c92c1dd4e18b3bf8ea5e
Gabriela Montenegro <91491595+Gasanmon@users.noreply.github.com>
2021-10-17 22:22:45 -0500

    Create Ejercicio19.py

---
+++ Tallerestructurasdecontrol/Ejercicio19.py
@@ -0,0 +1,20 @@
+"""
+Entradas:
+cantidad de naranjas ---> int ---> X
+precio por docena ---> float ---> Y
+valor venta ---> float ---> K
+Salidas:
+numero de docenas ---> float ---> docena
+costo ---> float ---> precio
+ganancia ---> float ---> ganancia
+porcentaje de ganancia ---> float ---> porcentaje
+
+"""
+X = int ( input ( "Numero de naranjas:" ))
+Y = float ( input ( "Valor por docena:" ))
+K = float ( input ( "Valor de las ventas:" ))
+docena = ( X / 12 )
+precio = ( Y * docena )
+ganancia = ( K - precio )
+porcentaje = (( ganancia * 100 ) / precio )
+print ( "El porcentaje de ganancias es:" + str ( porcentaje ), "%" )
