5956825b00a0399cadc80f659d547663d6d1bf0d
Gabriela Montenegro <91491595+Gasanmon@users.noreply.github.com>
2021-10-17 22:22:48 -0500

    Create Ejercicio2.py

---
+++ Tallerestructurasdecontrol/Ejercicio2.py
@@ -0,0 +1,10 @@
+"""
+Entradas
+Capital -> flotador -> Cap
+Salidas
+Ganancia -> flotar -> Gan
+
+"""
+Cap = ( float ( input ( "Digite el capital a invertir:" )))
+Gan = ( Cap * 0.02 )
+print ( "La ganancia es:" +  str ( Gan ), "Ganancia con el capital invertido:" +  str ( Gan + Cap ))
