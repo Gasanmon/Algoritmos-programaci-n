eb4b59afd93521c52c0b6d164b67d429824fcf34
Gabriela Montenegro <91491595+Gasanmon@users.noreply.github.com>
2021-10-17 22:22:30 -0500

    Create Ejercicio6.py

---
+++ Tallerestructurasdecontrol/Ejercicio6.py
@@ -0,0 +1,14 @@
+"""
+Entradas
+Mujeres -> int -> Mujeres
+Hombres -> int -> Hombres
+Salidas
+Total -> int -> Total
+
+"""
+Mujeres = int ( input ( "Numero de mujeres:" ))
+Hombres = int ( input ( "Numero de hombres:" ))
+Total = ( Mujeres + Hombres )
+PorMuj = ( Mujeres / Total * 100 )
+PorHom = ( Hombres / Total * 100 )
+print ( "Porcentaje de mujeres:" + str ( PorMuj ), "Porcentaje de hombres:" + str ( PorHom ))
