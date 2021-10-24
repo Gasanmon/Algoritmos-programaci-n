c6ffdf999c6ab7c1e7578e164fa6c59d1ed554c0
Gabriela Montenegro <91491595+Gasanmon@users.noreply.github.com>
2021-10-23 22:17:36 -0500

    Create Ejercicio1.py

---
+++ TallerEstructurasdeControlSelectivas/Ejercicio1.py
@@ -0,0 +1,20 @@
+"""
+Entrada
+
+Cantidad invertida --> float -->CI
+Tasa de intereses --> float -->TI
+Salida
+Intereses --> float -->INT
+ganancia total --> flotat --> total
+
+"""
+#Entrada
+CI = float ( input ( "Escriba la cantidad invertida:" ))
+TI = float ( input ( "Escriba la tasa de interes:" ))
+#Caja negra
+x = ( CI * TI ) / 100
+if (INT> 100.000 ):
+    total = CI  +  TI
+#Salida
+print ( "Los intereses son:" + str (INT))
+print ( "el salario total es:" + str ( total ))
