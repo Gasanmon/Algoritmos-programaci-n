1bbd12d03e6b80372a0be9b8efa54bf90cec5b9c
Gabriela Montenegro <91491595+Gasanmon@users.noreply.github.com>
2021-10-23 22:18:27 -0500

    Create Ejercicio16.py

---
+++ TallerEstructurasdeControlSelectivas/Ejercicio16.py
@@ -0,0 +1,32 @@
+"""
+Entradas
+A-->int-->A
+B-->int-->B
+C-->int-->C
+D-->int-->D
+Salidas
+X1-->int-->X1
+X2-->int-->X2
+"""
+#entrada
+f=input().split(" ")
+A,B,C,D=f
+A=int(A)
+B=int(B)
+C=int(C)
+#caja negra
+D=B**2-4*A*C
+import math
+if(D==0):
+  a=-B/(2*A)
+  X1=a
+  X2=a
+elif(D>0):
+  X1=(-B+math.sqrt(B**2-4*A*C))/(2*A)
+  X2=(-B+math.sqrt(B**2-4*A*C))/(2*A)
+elif(D<0):
+  X1="No tiene solucion"
+  X2="No tiene solucion"
+#salida
+print("X1 vale: "+str(X1))
+print("X2 vale: "+str(X2))
